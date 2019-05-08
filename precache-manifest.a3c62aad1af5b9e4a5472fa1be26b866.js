31b8c1ec866911d095cc108191650e380040c9c5
Brennan M <brennanmcvicar@gmail.com>
2019-05-08 12:48:14 -0400

    Updates

---
+++ precache-manifest.a3c62aad1af5b9e4a5472fa1be26b866.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ad9c5e624d874768f8a9c45f4df769",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "0f6a4f4c6f894a8375f4",
+    "url": "/connect-four/static/css/main.2c4bc429.chunk.css"
+  },
+  {
+    "revision": "8bdcd03b38a1a2937cf3",
+    "url": "/connect-four/static/js/2.8ec7c6f4.chunk.js"
+  },
+  {
+    "revision": "0f6a4f4c6f894a8375f4",
+    "url": "/connect-four/static/js/main.74ca62cc.chunk.js"
+  },
+  {
+    "revision": "5f78906a733342f2d383",
+    "url": "/connect-four/static/js/runtime~main.000e00dd.js"
+  }
+]);
